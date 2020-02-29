8649311c0df99270f21a77c0c17234fc543760be
Prebiusta <davidko123fr@gmail.com>
2020-02-29 14:20:03 +0100

    Reading Input & gitignore added

--- Lesson2/Exercise2/MathApp.c
+++ Lesson2/Exercise2/MathApp.c
@@ -2,6 +2,15 @@
 
 int main()
 {
-    printf("Hello David!\n");
+    char str[100];
+    int i;
+
+    printf("Hello David, enter a Value: ");
+
+    scanf("%d %s", &i, str);
+
+    printf("\nYou entered: %d__%s\n", i, str);
     return 0;
 }
+
+// Press F6 to compile and run
