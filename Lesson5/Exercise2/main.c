5442f541b0847fd3dfe7494b500110ffab3725d0
Prebiusta <davidko123fr@gmail.com>
2020-04-07 13:08:02 +0200

    Lesson 6 exercise 1 added

--- Lesson5/Exercise2/main.c
+++ Lesson5/Exercise2/main.c
@@ -1,10 +1,22 @@
+#include <stdio.h>
 #include "car.h"
 
 void main()
 {
     car_t myCar = car_create(360, "Tesla", "S");
 
-    printf("Power of a car: %d", car_getHp(myCar));
-    printf("Brand of the car: %s", car_getBrand(myCar));
-    printf("Model of the car: %s", car_getModel(myCar));
+    printf("Power of a car: %d\n", car_getHp(myCar));
+    printf("Brand of the car: %s\n", car_getBrand(myCar));
+    printf("Model of the car: %s\n", car_getModel(myCar));
+
+    printf("Chaning power of my car.\n");
+    car_tune(myCar, 400);
+    printf("New power of a car: %d\n", car_getHp(myCar));
+
+    car_start(myCar);
+    printf("Car is running: %d\n", car_isStarted(myCar));
+    car_stop(myCar);
+    printf("Car is running: %d\n", car_isStarted(myCar));
+
+    car_destroy(myCar);
 }
